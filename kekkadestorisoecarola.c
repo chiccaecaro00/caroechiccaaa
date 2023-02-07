312d53d87d889db88035d77e6d7d39b6b4d50338
chiccaecaro00 <124669060+chiccaecaro00@users.noreply.github.com>
2023-02-07 12:51:51 +0100

    Add files via upload

---
+++ kekkadestorisoecarola.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main ()
+{
+   
+
+    int a;
+    int b;
+   
+  printf("inserisci due numeri");
+
+  scanf("%d %d", &a, &b);
+
+   
+    if (a > b)
+    {
+printf("a è maggiore di b");
+    }
+    else if (a == b)
+    {
+printf("a è uguale a b");
+    }
+    else 
+    {
+printf("a è minore di b");
+    }
+
+
+
+}
