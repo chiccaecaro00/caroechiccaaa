1c39321e72db405cec3fea8a8183d7f3001db5c8
chiccaecaro00 <124669060+chiccaecaro00@users.noreply.github.com>
2023-02-07 10:29:23 +0100

    Add files via upload

---
+++ rampaecacca.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    char x = 'a';
+    char y = 'b' ;
+    int num = 55;
+
+    printf("x era: %c\n", x);
+    print
+
+    scanf("%d", &0);
+    printf ("x: %d\n", 0);
+    return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
