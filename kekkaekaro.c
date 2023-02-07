d31207f7c703e0c569b595fb07ee159e37da591c
chiccaecaro00 <124669060+chiccaecaro00@users.noreply.github.com>
2023-02-07 12:01:54 +0100

    Add files via upload

---
+++ kekkaekaro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+    int a;
+    int b;
+
+    printf("ïnserisci due numeri");
+    scanf ("%d %d", &a , &b);
+    int divisione = a / b;
+    printf("ïl risultato della divisione è: %d\n ", divisione);
+}
