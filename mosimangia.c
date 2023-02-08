c11e35e5cf0c6a4a22d8251dc32fd555bcab214a
chiccaecaro00 <124669060+chiccaecaro00@users.noreply.github.com>
2023-02-08 12:55:15 +0100

    Add files via upload

---
+++ mosimangia.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main ()
+{
+    int x=0; 
+    int a=2;
+    while(a<=100)
+    
+    {
+        x=x+a;
+        a=a+2;
+        
+
+
+
+    }
+
+
+
+    printf("la somma: %d\n",x);
+
+
+}
